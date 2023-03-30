816848ae49d00ef1a0956d5bd58088f0d1e06525
Alison Orton <orton.alison@gmail.com>
2023-03-29 19:48:22 -1000

    Populate map

--- decomp.cpp
+++ decomp.cpp
@@ -4,8 +4,13 @@
 #include <sstream>
 #include <vector>
 #include <map>
+#include <iterator> 
+#include <algorithm> 
 
 using namespace std;
+
+map <string, string> input;
+vector <string> lines;
 
 /*****************************************************
  * MAIN
@@ -17,6 +22,7 @@ int main(int argc, char** argv)
         std::cout << "You forgot the file name." << endl;
         exit(0);
     }
+
     ifstream ifs;
     ifs.open(argv[1], ios::in);
     if (!ifs) {
@@ -24,5 +30,50 @@ int main(int argc, char** argv)
         exit(0);
     }
 
-    
+    string tmp;
+    string key;
+    string value;
+    //getline(ifs, tmp, ' ');
+    getline(ifs, tmp);
+
+    cout << tmp << endl;
+    bool stop = false;
+
+    //Populating vector
+    while (tmp[0] != '*'){
+        lines.push_back(tmp);
+        getline(ifs, tmp);
+    }
+
+    //Spliting lines in vector and populating map
+    for(int i = 0; i < lines.size(); i++){
+        //cout << "in the FOR loop" << endl;
+        string test = lines[i];
+        char separator = ' ';
+        int j = 0;
+        string s;
+
+        for(int k = 0; k < test.size(); k++){
+            if(test[k] != separator){
+                s += test[k];
+            }
+            else {
+                key = s;
+                s.clear();
+            }
+        }
+            value = s;
+            cout << "Key: " << key << " Char: " << value << endl;
+
+            input[key] = value;
+
+            cout << lines[i] << endl;
+        }
+
+
+        int bits;
+        getline(ifs, tmp);
+        bits = stoi(tmp); //Num of bits
+        cout << bits << endl;
+        
 }
